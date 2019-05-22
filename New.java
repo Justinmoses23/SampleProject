5e7b531c1ce7c7b87e2bd6a936e7d8d222701344
justinmoses23 <justinmoses23@gmail.com>
2019-05-22 07:31:40 +0530

    code added

---
+++ New.java
@@ -0,0 +1,17 @@
+
+public class New {
+	public static void main(String[]agrs) {
+		int a =30;
+		int b=20;
+		if(a==b)
+		{
+			System.out.println("both are same");
+		}else if(a>=b)
+		{
+			System.out.println("A");
+		}else if(b>=a) {
+		System.out.println("B");
+		}
+	}
+
+}
